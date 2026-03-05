41c57edcddfb47ac8ff331d8cfe227ab624056bc
Fnbeii <146079808+fnbeii@users.noreply.github.com>
2026-03-05 15:44:19 +0700

    Rename module

--- Android.mk
+++ Android.mk
@@ -3,9 +3,9 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 LOCAL_CPP_EXTENSION := .cpp .cc
 ifeq ($(TARGET_ARCH_ABI), armeabi-v7a)
-    LOCAL_MODULE    := MoneySeparator
+    LOCAL_MODULE    := ColorMarkerSAMP
 else
-    LOCAL_MODULE    := MoneySeparator64
+    LOCAL_MODULE    := ColorMarkerSAMP64
 endif
 LOCAL_SRC_FILES := main.cpp mod/logger.cpp mod/config.cpp
 LOCAL_CFLAGS += -O2 -mfloat-abi=softfp -DNDEBUG -std=c++17
